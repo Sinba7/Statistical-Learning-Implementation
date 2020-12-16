94c0288e45edd00dfd45b1891888efe9e587e717
Sinba7 <zhangny3@gmail.com>
2020-12-15 22:28:30 -0800

    clean up movie recom project

--- projects/Movie-Recommendation-System/startup.sh
+++
@@ -1,5 +0,0 @@
-echo 'istart nstalling required packages'
-pip install -r requirements.txt
-echo 'successfully installed required packages'
-echo 'start the movie recommendation app'
-python src/dash_app.py
